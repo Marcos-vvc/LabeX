5e03723a9dc57badc3f7df2057a52fc9c1012db5
marcos <marcos-dido@hotmail.com>
2022-06-16 19:15:42 -0300

    Projeto LabeX

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import axios from "axios";
+import { Router } from "./routes/Router";
+
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Router />
+    </div>
+  );
+}
+
+export default App;
